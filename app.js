c32c5c595ff8811754f67091a9cbe49158b01291
UtkirMansurovich <81430269+UtkirMansurovich@users.noreply.github.com>
2021-06-01 14:35:07 +0500

    type chenges

---
+++ app.js
@@ -0,0 +1,11 @@
+const navbarNav = document.querySelector('.navbarNav');
+function openNavbar(){
+    navbarNav.classList.add('open');
+}
+const close = document.querySelector('.navbarNav');
+function shut(){
+    navbarNav.classList.remove('open');
+}
+
+
+    
